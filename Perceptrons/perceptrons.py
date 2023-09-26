401106c69affe510918cfda80245f086e55c44e4
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2023-09-26 00:01:09 -0300

    feat: finalizando exemplo

--- Perceptrons/perceptrons.py
+++ Perceptrons/perceptrons.py
@@ -5,18 +5,18 @@ baias = 0
 limit = 3
 
 print("""-------------------------------------------
-Responda as Perguntas a seguir com S ou N, dado ao evento a seguir...
+Responda às perguntas a seguir com S ou N, dado ao evento a seguir...
 Haverá um evento familiar, e há um levantamento de requisitos para a sua ida,
-responda as questões e observe o resultado.
+responda às questões e observe o resultado.
 -------------------------------------------""")
       
 res = str.upper(input("Você conferiu a previsão do tempo, o tempo vai estar bom ? "))
 x1 = w if res == "S" else 0
 
-res = str.upper(input("Você esta namorando, o seu namorado(a) vai ir junto com você ? "))
+res = str.upper(input("Você está namorando, o seu namorado(a) vai ir junto com você ? "))
 x2 = w2 if res == "S" else 0
 
-res = str.upper(input("Você não tem viculo, e há a necessidade de ir via transporte publico, o local do evento é proximo a linhas de transporte publicos ? "))
+res = str.upper(input("Você não tem vículo, e há a necessidade de ir via transporte público, o local do evento é próximo a linhas de transporte públicos ? "))
 x3 = w2 if res == "S" else 0
 
 if((x1 + x2 + x3)+ baias > limit):
